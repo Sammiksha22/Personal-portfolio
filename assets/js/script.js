3bf32dfef93ad5a3baf3bbd72c7b90c072b0a4cc
Sammiksha22 <kobalsamiksha7@gmail.com>
2024-12-03 15:33:10 +0530

    g

--- assets/js/script.js
+++ assets/js/script.js
@@ -84,7 +84,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["Front-end developer", "Python Developer", "Software Developer"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
